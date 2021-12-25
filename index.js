6094c7192e18fbf2f281a225ae8fef8c61e5b6bd
Chase Galatro <chase@Chases-MacBook-Air.local>
2021-12-25 11:44:00 -0500

    removed redux

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import 'react-native-gesture-handler';
-import { registerRootComponent } from 'expo';
+import React from "react";
+import "react-native-gesture-handler";
+import { registerRootComponent } from "expo";
 
-import App from './App';
+import App from "./App";
+
+const toknWatch = () => <App />;
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(toknWatch);
